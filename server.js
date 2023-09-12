1ba7b6b5d05e1219e024163965073081cf3abd52
Akash Singh Naruka <akashnaruka01@gmail.com>
2023-09-12 17:19:54 +0530

    frontend url problem

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { connectDB } from "./data/database.js";
 
 connectDB();
 
+
 app.listen(process.env.port, () => {
   console.log(`server is working on port ${process.env.port} in ${process.env.NODE_ENV} mode`);
 }); 
