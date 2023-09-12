86a087875eb51610109bb36a58516ea6b7803d30
Akash Singh Naruka <akashnaruka01@gmail.com>
2023-09-12 18:21:57 +0530

    errormiddleware bug

--- controler/middlewares/error.js
+++ controler/middlewares/error.js
@@ -1,21 +1,19 @@
-class errorHandler extends Error {         // own made class for status code transfer.
-    constructor(message, statusCode) {
-        super(message);
-        this.statusCode = statusCode;
-    }
+class errorHandler extends Error {
+  // own made class for status code transfer.
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
 }
 
 export const errormiddleware = () => {
+  error.message = error.message || "Internal Server Error";
+  error.statusCode = error.statusCode || 500;
 
-    error.message = error.message || "Internal Server Error";
-    error.statusCode = error.statusCode || 500;
-
-    app.use((error,req,res,next)=>{
-        res.statusCode(error.statusCode).json({
-          success: false,
-          message: error.message,
-        })
-      })
-}
+  return res.statusCode(error.statusCode).json({
+    success: false,
+    message: error.message,
+  });
+};
 
 export default errorHandler;
